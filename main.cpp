0e6788aa3678bb0c46f1d9dae09a9170b837d9f0
Marcus Bernroth <marcusbernroth@gmail.com>
2022-11-12 13:15:00 +0100

    Configuring linter

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
-    qInfo() << "Hello world!";
+    int age = 44;
+    double height = 6.02;
+    qInfo() << "Age:" << age;
+    qInfo() << "Height:" << height;
+    age = 44.90;
     return app.exec();
 }
