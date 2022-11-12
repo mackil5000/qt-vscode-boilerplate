33f897b14a76f33c4e84c5caace6aa5434ac3acc
Marcus Bernroth <marcusbernroth@gmail.com>
2022-11-12 14:48:05 +0100

    Section complet

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
+#include <iostream>
 #include <QCoreApplication>
 #include <QDebug>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
-    qInfo() << "Hello world!";
+
+    int age = 0;
+    qInfo() << "Enter your age";
+    cin >> age;
+    qInfo() << "Your are" << age << "years old";
+
+    qInfo() << "Information";
+    qDebug() << "Debug";
+    qCritical() << "Critical";
+    qWarning() << "Warning";
+
     return app.exec();
 }
